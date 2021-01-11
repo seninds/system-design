84fbc500f866f506c13c87f170e5776a67fa6d76
Dmitrii Senin <seninds@gmail.com>
2021-01-11 23:15:32 +0000

    Add trivial impl for InProcessCache (without TTL)

--- problems/url_shortener/code/url_shortener/lib/in_process_cache.cc
+++ problems/url_shortener/code/url_shortener/lib/in_process_cache.cc
@@ -1,12 +1,18 @@
 #include "in_process_cache.h"
 
+#include "absl/strings/str_cat.h"
+
 namespace us {
 
 absl::StatusOr<ICacheStorage::Record> InProcessCache::Read(const std::string& key) const {
-    return ICacheStorage::Record().WithShortUrl("fake_short_url").WithLongUrl("fake_long_url");
+    if (auto it = data_.find(key); it != data_.end()) {
+        return it->second;
+    }
+    return absl::NotFoundError(absl::StrCat("couldn't find key ", key, " in cache"));
 }
 
 absl::Status InProcessCache::Write(const std::string& key, ICacheStorage::Record record) {
+    data_[key] = std::move(record);
     return absl::OkStatus();
 }
 
--- problems/url_shortener/code/url_shortener/lib/in_process_cache.h
+++ problems/url_shortener/code/url_shortener/lib/in_process_cache.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <unordered_map>
+
 #include "inc/cache_storage.h"
 
 namespace us {
@@ -7,6 +10,9 @@ namespace us {
 struct InProcessCache : ICacheStorage {
     absl::StatusOr<ICacheStorage::Record> Read(const std::string& key) const override;
     absl::Status Write(const std::string& key, ICacheStorage::Record record) override;
+
+   private:
+    std::unordered_map<std::string, ICacheStorage::Record> data_;
 };
 
 }  // namespace us
